15a5fb82ef57918d8254f6603694e0406a3b6190
juanvalentin <valentin.juan05@gmail.com>
2023-05-23 16:25:39 -0300

    Inicial tests with playwright

---
+++ tests/tasks.spec.ts
@@ -0,0 +1,19 @@
+import { test, expect } from "@playwright/test";
+
+test('deve poder cadastrar uma nova tarefa', async ({ page }) =>{
+    await page.goto('http://localhost:3000');length
+
+    const inputTaskName = page.locator('input[class*=InputNewTask]')
+    await inputTaskName.fill('Task 1');
+    // await page.fill('#newTask', 'Task 1');
+    // await page.fill('input[placeholder="Add a new Task"]', 'Task 1');
+    // await page.fill('input[type=text]', 'Task 1');
+
+    // Valores '_1y0mp_21' certamente são gerados dinamicamente
+    // await page.fill('._listInputNewTask_1y0mp_21', 'Task 1');
+
+    // Alteração com um 'regex' para pegar apenas a parte 'fixa' do class
+    await page.fill('input[class*=InputNewTask]', 'Task 1');
+
+
+})
